0bcffadf3fe6d7ee296babd46e0fa9bf34868bf8
spallya <spallya@outlook.com>
2020-03-01 20:10:59 +0530

    Refactored

--- src/main/java/linkedList/JediLinkedListTest.java
+++ src/main/java/linkedList/JediLinkedListTest.java
@@ -2,23 +2,23 @@ package linkedList;
 
 public class JediLinkedListTest {
 
-	public static JediLinkedList jediLinkedList;
+	public static JediLinkedList<Integer> jediLinkedList;
  
 	public static void main(String[] args) {
-		jediLinkedList = new JediLinkedList<Integer>();
+		jediLinkedList = new JediLinkedList<>();
 		jediLinkedList.add(110);
 		jediLinkedList.add(221);
 		jediLinkedList.add(13);
 		jediLinkedList.add(44);
 		jediLinkedList.add(575);
-		System.out.println("List: \t\t" + jediLinkedList);
+		System.out.println("List: \t\t\t\t\t" + jediLinkedList);
 		System.out.println(".size(): \t\t\t\t" + jediLinkedList.size());
 		System.out.println(".get(3): \t\t\t\t" + jediLinkedList.get(3) + " (get element at index:3 - list starts from 0)");
-		System.out.println(".remove(2): \t\t\t\t" + jediLinkedList.remove(2) + " (element removed)");
-		System.out.println("List: \t\t" + jediLinkedList);
+		System.out.println(".remove(2): \t\t\t" + jediLinkedList.remove(2) + " (element removed)");
+		System.out.println("List: \t\t\t\t\t" + jediLinkedList);
 		System.out.println(".get(3): \t\t\t\t" + jediLinkedList.get(3) + " (get element at index:3 - list starts from 0)");
 		System.out.println(".size(): \t\t\t\t" + jediLinkedList.size());
-		System.out.println("List: \t" + jediLinkedList);
+		System.out.println("List: \t\t\t\t\t" + jediLinkedList);
 	}
  
 }
