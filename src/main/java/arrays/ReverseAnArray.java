8d7243256f733587fd8f2167d42739bd97c2373e
spallya <spallya@outlook.com>
2020-03-01 18:11:28 +0530

    Reversing an array

--- src/main/java/arrays/ReverseAnArray.java
+++ src/main/java/arrays/ReverseAnArray.java
@@ -1,3 +1,30 @@
 package arrays;
 
-public class ReverseAnArray {}
+public class ReverseAnArray {
+
+    public static void main(String[] args) {
+        int [] input = {10, 20, 30, 40, 50};
+        System.out.println("Current array: ");
+        printArray(input);
+        System.out.println("\nReversed array: ");
+        printArray(reverse(input));
+    }
+
+    private static void printArray(int[] input) {
+        for (int i : input) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static int[] reverse(int[] input) {
+        int length = input.length;
+        int  temp;
+        for (int i = 0; i < length/ 2; i++) {
+            temp = input[i];
+            input[i] = input[length - i - 1];
+            input[length - i - 1] = temp;
+        }
+        return input;
+    }
+
+}
