8c4c7f79fc89f0a17aa62ad878e247d331c2750a
spallya <spallya@outlook.com>
2020-03-17 20:46:47 +0530

    Added isEmpty method and refactor

--- src/main/java/stacks/StackUsingArray.java
+++ src/main/java/stacks/StackUsingArray.java
@@ -43,26 +43,42 @@ public class StackUsingArray<T> {
         return sb.toString();
     }
 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     /*
     * Program Output:
     *
-    * [40, 30, 20, 10]
-    * 40
-    * 30
-    * 20
-    * [10]
+    * Is Stack Empty: true
+    * Current Stack: [40, 30, 20, 10]
+    * Is Stack Empty: false
+    * Element Popped: 40
+    * Element Popped: 30
+    * Element Popped: 20
+    * Current Stack: [10]
+    * Is Stack Empty: false
+    * Element Popped: 10
+    * Current Stack: []
+    * Is Stack Empty: true
     *
     * */
     public static void main(String[] args) {
         StackUsingArray<Integer> stack = new StackUsingArray<>();
+        System.out.println("Is Stack Empty: " + stack.isEmpty());
         stack.push(10);
         stack.push(20);
         stack.push(30);
         stack.push(40);
-        System.out.println(stack);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack);
+        System.out.println("Current Stack: " + stack);
+        System.out.println("Is Stack Empty: " + stack.isEmpty());
+        System.out.println("Element Popped: " + stack.pop());
+        System.out.println("Element Popped: " + stack.pop());
+        System.out.println("Element Popped: " + stack.pop());
+        System.out.println("Current Stack: " + stack);
+        System.out.println("Is Stack Empty: " + stack.isEmpty());
+        System.out.println("Element Popped: " + stack.pop());
+        System.out.println("Current Stack: " + stack);
+        System.out.println("Is Stack Empty: " + stack.isEmpty());
     }
 }
